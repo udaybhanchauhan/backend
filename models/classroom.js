a4183848d705f4f581acde679c00cc86c1c56416
udaybhanc <udaybhan10790@gmail.com>
2019-02-20 18:04:38 +0900

    second comment

---
+++ models/classroom.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Classroom = sequelize.define('Classroom', {
+    class_name: DataTypes.STRING
+  }, {});
+  Classroom.associate = function(models) {
+    // associations can be defined here
+    Classroom.hasMany(models.Student,{
+      foreignKey:'classroom_id',
+      as:'students',
+    });
+  };
+  return Classroom;
+};
